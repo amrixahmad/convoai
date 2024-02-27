235271d2c3ccc9009e49deca442d03875929c19d
amrixahmad <amri.ahmad85@gmail.com>
2024-02-27 21:34:26 +0800

    completed chat functionality

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css,bot_template,user_template
 
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
@@ -32,11 +36,45 @@ def get_vector_store(text_chunks):
     vector_store=FAISS.from_texts(texts=text_chunks,embedding=embeddings)
     return vector_store
 
+def get_conversation_chain(vector_store):
+    llm=ChatOpenAI()
+    memory=ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+    conversation_chain=ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handleUserInput(user_question):
+    response=st.session_state.conversation({'question':user_question})
+    # st.write(response)
+    st.session_state.chat_history=response["chat_history"]
+    # st.write(st.session_state.chat_history)
+
+    for i, message in enumerate(st.session_state.chat_history):
+        
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}",message.content),unsafe_allow_html=True)
+
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Chat with multiple pdfs",page_icon=":books:")
+    load_dotenv()    
+    st.set_page_config(page_title="Chat with multiple pdfs",page_icon=":books:")    
+    st.write(css,unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    
     st.header("Chat with multiple pdfs :books:")
-    st.text_input("Ask a question about your documents:")
+    user_question=st.text_input("Ask a question about your documents:")
+
+    if user_question:
+        handleUserInput(user_question)    
     
     with st.sidebar:
         st.subheader("Your documents:")
@@ -48,6 +86,8 @@ def main():
                 text_chunks=get_text_chunks(raw_text)
                 # st.write(text_chunks)
                 vector_store=get_vector_store(text_chunks)
+                # st.write(type(vector_store))
+                st.session_state.conversation=get_conversation_chain(vector_store)
     
     # print(OPENAI_API_KEY)
 
